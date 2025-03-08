bfc60f7a974cff21fac17e8224a43583d6bcd825
mishrasumit18899 <sumitmishra13499@gmail.com>
2025-03-08 12:57:46 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -1,22 +1,12 @@
-package in.javahome.myweb.controller;
-/*
- * 
- */
-public class Calculator {
-	
-	/*
-	 * @param i
-	 * @param j
-	 * @return int
-	 */
-	public int add(int i, int j){
-		return i+j;
-	}
-	public int multiply(int i, int j){
-		return i*j;
-	}
-	
-	public int sub(int i, int j){
-		return i-j;
-	}
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
 }
+
